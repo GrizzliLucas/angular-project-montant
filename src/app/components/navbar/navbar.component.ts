7e2c94ff77e42a028e63e6c4900cd9dbd8a75517
Lucas <lucas@MacBook-Pro-de-Lucas.local>
2023-05-11 12:19:24 +0200

    traduction working

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private translateService: TranslateService) {
+    translateService.setDefaultLang(this.translateService.getDefaultLang());
+    translateService.use(this.translateService.getDefaultLang());
+  }
+
   hovering = false;
+
   logout() {
     this.authService.signOut();
+  }
+
+  changeLanguage(lang: string) {
+    this.translateService.use(lang);
   }
 }
